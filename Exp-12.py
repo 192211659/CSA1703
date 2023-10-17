0c82f4c3ebb284eb6fa341325892a0fc02c01bac
192211659 <124418149+192211659@users.noreply.github.com>
2023-10-17 13:27:20 +0530

    Exp-12.py

--- Exp-12.py
+++ Exp-12.py
@@ -1,57 +1,39 @@
-def sum(a,b,c):
-    return a+b+c
-def printBoard(xState , zState):
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
 
-    zero =  'X' if xState[0] else('@' if zState[0] else 0 )
-    one =  'X' if xState[1] else('@' if zState[1] else 1 )
-    two =  'X' if xState[2] else('@' if zState[2] else 2 )
-    three =  'X' if xState[3] else('@' if zState[3] else 3 )
-    four =  'X' if xState[4] else('@' if zState[4] else 4 )
-    five =  'X' if xState[5] else('@' if zState[5] else 5 )
-    six =  'X' if xState[6] else('@' if zState[6] else 6 )
-    seven =  'X' if xState[7] else('@' if zState[7] else 7 )
-    eight =  'X' if xState[8] else('@' if zState[8] else 8 )
 
-    print(f" {zero} | {one} | {two} ")
-    print(f"---|---|---")
-    print(f" {three} | {four} | {five} ")
-    print(f"---|---|---")
-    print(f" {six} | {seven} | {eight} ")
+def check_winner(board):
+    for row in board + list(zip(*board)):
+        if all(cell == row[0] and cell != " " for cell in row):
+            return True
+    if all(board[i][i] == board[0][0] and board[i][i] != " " for i in range(3)) or \
+       all(board[i][2 - i] == board[0][2] and board[i][2 - i] != " " for i in range(3)):
+        return True
+    return False
 
-def checkWin(xState,zState) : 
-    wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-    for win in wins :
-        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3) :
-            print("X won the game")
-            return 1
-        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3) :
-            print("O won the game.")
-            return 0
-    return -1
 
-if __name__ == "__main__" : 
+def play_game():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    player = "X"
+    
+    while any(" " in row for row in board):
+        print_board(board)
+        row, col = map(int, input(f"Player {player}, enter row and column (e.g., 1 1): ").split())
+        if board[row - 1][col - 1] == " ":
+            board[row - 1][col - 1] = player
+            if check_winner(board):
+                print_board(board)
+                print(f"Player {player} wins!")
+                return
+            player = "O" if player == "X" else "X"
+        else:
+            print("Cell already taken. Try again.")
+    
+    print_board(board)
+    print("It's a draw!")
 
-    total_turns = 9
 
-    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0 ]
-    turn = 1 # 1 for X and 0 for O
-    print("Welcome to TIC-TAC-TOE")
-    while(True) : 
-        printBoard(xState, zState)
-        if(turn == 1):
-            print("X's Chance")
-            value = int(input("Please enter a value : "))
-            xState[value] = 1
-        else :
-            print("O's Chance")
-            value = int(input("Please enter a value : "))
-            zState[value] = 1
-
-        total_turns = total_turns - 1
-
-        if(checkWin(xState, zState) != -1 or total_turns == 0 ):
-            print("GAME OVER")
-            printBoard(xState, zState)
-            break
-        turn = 1 -  turn 
+if __name__ == "__main__":
+    play_game()
